dcab2139eb86913e0839c0292d0786a40822e22f
Nathan Mustaki <feydaykyn@gmail.com>
2021-04-14 23:54:31 +0200

    FIX create_slots command

--- mysite/registrator/management/commands/create_slots.py
+++ mysite/registrator/management/commands/create_slots.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
         duration = timedelta(hours=1, minutes=30)
 
         the_5 = timezone.localtime().replace(year=2021, month=6, day=5, hour=10, minute=0, second=0, microsecond=0)
-        the_5_max = timezone.localtime().replace(year=2021, month=6, day=5, hour=18, minute=30, second=0, microsecond=0)
+        the_5_max = timezone.localtime().replace(year=2021, month=6, day=5, hour=19, minute=0, second=0, microsecond=0)
 
         while the_5 < the_5_max:
             slot = TimeSlot.objects.create(duration=duration, max_seats=15, time=the_5)
@@ -19,7 +19,7 @@ class Command(BaseCommand):
             the_5 = the_5 + timedelta(minutes=30)
 
         the_6 = timezone.localtime().replace(year=2021, month=6, day=6, hour=10, minute=0, second=0, microsecond=0)
-        the_6_max = timezone.localtime().replace(year=2021, month=6, day=6, hour=18, minute=30, second=0, microsecond=0)
+        the_6_max = timezone.localtime().replace(year=2021, month=6, day=6, hour=19, minute=0, second=0, microsecond=0)
 
         while the_6 < the_6_max:
             slot = TimeSlot.objects.create(duration=duration, max_seats=15, time=the_6)
